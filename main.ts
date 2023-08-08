84eb0755bd22cb4016f472816d06a90219e0eea9
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2023-08-08 17:11:50 +0900

    Feat error type

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+// @ts-ignore
+import { Result, Success, Failure } from "./errors.ts";
+
+function foo(): Result<string, Error> {
+  return new Success("success");
+}
+
+function bar(): Result<string, Error> {
+  return new Failure(new Error("bar"));
+}
+
+(() => {
+  const result = foo();
+
+  if (result.isFailure()) {
+    console.log(result.error);
+    return;
+  }
+
+  console.log(result.value);
+})();
