77f0188414b96b41038dec195905d30815c05100
Masataka Hisasue <masataka.hisasue@knocknote.co.jp>
2023-08-08 18:22:51 +0900

    Feat wrap async example

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import { Result, Success, Failure, tryCatch } from "./errors.ts";
+import { Result, Success, Failure, tryCatch, tryCatchAsync } from "./errors.ts";
 
 function foo(): Result<string, Error> {
   return new Success("success");
@@ -9,12 +9,12 @@ function bar(): Result<string, Error> {
   return new Failure(new Error("bar"));
 }
 
-function hoge(): string {
-  throw new Error("foo");
+function hoge(message: string): string {
+  throw new Error(message);
 }
 
-function piyo(message: string): string {
-  throw new Error(message);
+async function piyo(): Promise<string> {
+  return "piyo";
 }
 
 (() => {
@@ -33,12 +33,25 @@ function piyo(message: string): string {
 
   const result = tryCatch(
     () => {
-      return piyo(message);
+      return hoge(message);
     },
     (e: unknown) => {
       return e as Error;
     }
   );
+
+  if (result.isFailure()) {
+    console.log(result.error);
+    return;
+  }
+
+  console.log(result.value);
+})();
+
+(async () => {
+  const result = await tryCatchAsync(piyo, (e: unknown) => {
+    return e as Error;
+  });
 
   if (result.isFailure()) {
     console.log(result.error);
